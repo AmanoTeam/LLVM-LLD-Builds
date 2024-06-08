f5520a248e4fb581e6a496e936816354e5a41fc6
Kartatz <105828205+Kartatz@users.noreply.github.com>
2024-06-08 13:12:53 -0300

    Update to LLD 18.1.7

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -eu
 
 declare -r workdir="${PWD}"
 
-declare -r llvm_version='18.1.6'
+declare -r llvm_version='18.1.7'
 
 declare -r llvm_tarball='/tmp/llvm.tar.gz'
 declare -r llvm_directory="/tmp/llvm-project-llvmorg-${llvm_version}"
